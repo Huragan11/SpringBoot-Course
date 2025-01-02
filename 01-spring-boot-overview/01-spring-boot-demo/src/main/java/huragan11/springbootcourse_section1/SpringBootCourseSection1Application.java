808a319b5bbb8fd17968830faf4a797a14e4c4eb
Huragan11 <hubert.ragan11@gmail.com>
2025-01-02 20:44:57 +0100

    changing dir

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/huragan11/springbootcourse_section1/SpringBootCourseSection1Application.java
@@ -0,0 +1,13 @@
+package huragan11.springbootcourse_section1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseSection1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCourseSection1Application.class, args);
+    }
+
+}
