1ebf54f5932e590098459a4896ca20868158219f
Huragan11 <hubert.ragan11@gmail.com>
2025-01-03 14:19:48 +0100

    add server configuration; add value injection

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/huragan11/springbootcourse_section1/rest/FunRestController.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/huragan11/springbootcourse_section1/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package huragan11.springbootcourse_section1.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
 
     @GetMapping("/hello")
     public String sayHello(){
@@ -20,5 +32,4 @@ public class FunRestController {
     public String getDailyFortune(){
         return "Today is your lucky day.";
     }
-
 }
