7a4ff6d93ce495992505d94249402a5e3b82ca44
Huragan11 <hubert.ragan11@gmail.com>
2025-01-07 18:40:25 +0100

    Add controller and additional classes

---
+++ springcoredemo/src/main/java/huragan11/springcoredemo/common/Coach.java
@@ -0,0 +1,6 @@
+package huragan11.springcoredemo.common;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ springcoredemo/src/main/java/huragan11/springcoredemo/rest/DemoController.java
@@ -0,0 +1,28 @@
+package huragan11.springcoredemo.rest;
+
+import huragan11.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    private DemoController(Coach coach) {
+        myCoach = coach;
+    }
+
+
+    @GetMapping("/dailyworkout")
+    public String getWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+
+}
+
+
+
