72bcda405ebc2acdb59cdfe2fcbda9a2339c2219
Huragan11 <hubert.ragan11@gmail.com>
2025-01-14 14:42:13 +0100

    Add Student DAO; Add creating and saving on start

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/huragan11/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/huragan11/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package huragan11.cruddemo;
 
+import huragan11.cruddemo.dao.StudentDAO;
+import huragan11.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,20 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
-		return runner -> {
-			System.out.println("Hello world from a runner");
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return runner -> {
+            createStudent(studentDAO);
+        };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        Student student = new Student("John", "Doe", "johndoe@gmail.com");
+        studentDAO.save(student);
+        System.out.println("Student created, saved on id " + student.getId());
+    }
 }
---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/huragan11/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package huragan11.cruddemo.dao;
+
+import huragan11.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
