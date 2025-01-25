fe8327ad709224b7e215753e52f02d7167244ccd
Huragan11 <hubert.ragan11@gmail.com>
2025-01-25 11:24:33 +0100

    Add controllers and Student class

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/huragan11/demo/rest/StudentRestController.java
@@ -0,0 +1,38 @@
+package huragan11.demo.rest;
+
+import huragan11.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("John","Smith"));
+        theStudents.add(new Student("James","Doe"));
+        theStudents.add(new Student("Alice","Smith"));
+        theStudents.add(new Student("Marry","Doe"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
+    }
+}
