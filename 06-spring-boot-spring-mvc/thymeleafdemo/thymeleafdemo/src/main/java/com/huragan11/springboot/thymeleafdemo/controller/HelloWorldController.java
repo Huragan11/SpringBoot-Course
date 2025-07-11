cfb6ba147353672c60b2f94d608093f9ffafee90
Huragan11 <hubert.ragan11@gmail.com>
2025-07-11 07:15:38 +0200

    Change request annotation

--- 06-spring-boot-spring-mvc/thymeleafdemo/thymeleafdemo/src/main/java/com/huragan11/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/thymeleafdemo/thymeleafdemo/src/main/java/com/huragan11/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.huragan11.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -32,7 +34,7 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/newShout")
+    @PostMapping("/newShout")
     public String newShout(@RequestParam("studentName") String name, Model model){
 
         name = name.toUpperCase();
