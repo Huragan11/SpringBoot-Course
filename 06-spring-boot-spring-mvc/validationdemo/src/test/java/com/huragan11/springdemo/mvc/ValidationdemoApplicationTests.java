7bfc91e04b6ef6a730dfaf288ca4e5fa4dad1c83
Huragan11 <hubert.ragan11@gmail.com>
2025-07-13 08:36:22 +0200

    add new subproject

---
+++ 06-spring-boot-spring-mvc/validationdemo/src/test/java/com/huragan11/springdemo/mvc/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huragan11.springdemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
