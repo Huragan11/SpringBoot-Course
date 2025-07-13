a5dddaac57669fa01605f5beb460a53af6a685c2
Huragan11 <hubert.ragan11@gmail.com>
2025-07-13 10:47:50 +0200

    Add String trimmer for validation

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/huragan11/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/huragan11/springdemo/mvc/CustomerController.java
@@ -1,15 +1,26 @@
 package com.huragan11.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmer = new StringTrimmerEditor(true);
+
+        binder.registerCustomEditor(String.class, stringTrimmer);
+    }
 
     @GetMapping("/")
     public String showForm(Model model){
@@ -21,6 +32,8 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult result){
+
+        System.out.println("Last name: " + customer.getLastName());
 
         if(result.hasErrors()){
             return "customer-form";
