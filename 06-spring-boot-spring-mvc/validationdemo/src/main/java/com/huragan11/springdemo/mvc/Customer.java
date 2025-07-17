182ad09bfae480547ad48768d7eff81ee8386cd1
Huragan11 <hubert.ragan11@gmail.com>
2025-07-17 06:46:11 +0200

    Updated freePasses to be Integer and NotNull

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/huragan11/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/huragan11/springdemo/mvc/Customer.java
@@ -10,9 +10,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "cannot be lower than 0")
     @Max(value = 10, message = "cannot be greater than 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
     private String postalCode;
@@ -41,11 +42,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 }
