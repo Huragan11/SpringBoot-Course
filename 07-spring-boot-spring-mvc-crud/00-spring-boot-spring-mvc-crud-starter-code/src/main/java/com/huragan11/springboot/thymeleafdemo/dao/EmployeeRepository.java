133983cb0de10e0d08682038f09ecb651748cf35
Huragan11 <hubert.ragan11@gmail.com>
2025-08-02 11:08:33 +0200

    Made table show sorted people by last name

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/huragan11/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/huragan11/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,6 +3,10 @@ package com.huragan11.springboot.thymeleafdemo.dao;
 import com.huragan11.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
