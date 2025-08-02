9581a15170f5070dc92cb0e911290326398a572c
Huragan11 <hubert.ragan11@gmail.com>
2025-08-02 11:09:33 +0200

    Add get back link from form and save employee endpoint

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/huragan11/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/huragan11/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.huragan11.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,13 +28,21 @@ public class EmployeeController {
         return "employees/list-employees";
     }
 
-    @GetMapping("/showFromForAdd")
+    @GetMapping("/showFormForAdd")
     public String showFromForAdd(Model model) {
 
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "employees/employee-form";
     }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
+    }
+
 
 }
 
