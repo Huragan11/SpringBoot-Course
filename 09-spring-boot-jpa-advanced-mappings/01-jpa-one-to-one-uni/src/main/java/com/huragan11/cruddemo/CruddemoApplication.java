6e34fb2a796d9b4a8793744b621ab8cfbb8e1bd8
Huragan11 <hubert.ragan11@gmail.com>
2025-10-11 13:30:59 +0200

    App prep and detail class added

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/huragan11/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/huragan11/cruddemo/CruddemoApplication.java
@@ -1,13 +1,19 @@
 package com.huragan11.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> System.out.println("hello!");
+    }
 }
