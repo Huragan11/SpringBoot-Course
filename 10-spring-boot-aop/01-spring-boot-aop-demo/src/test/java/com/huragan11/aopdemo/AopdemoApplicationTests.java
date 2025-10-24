f9c56be7209bec6785d5517c54d8e8df67588385
Huragan11 <hubert.ragan11@gmail.com>
2025-10-24 07:16:43 +0200

    add new subproject for AOP

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/test/java/com/huragan11/aopdemo/AopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huragan11.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
