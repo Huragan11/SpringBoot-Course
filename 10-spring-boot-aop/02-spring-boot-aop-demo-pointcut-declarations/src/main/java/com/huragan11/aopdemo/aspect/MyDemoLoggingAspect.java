f3add4e15aeaab9e804e3dc6351059ff599cbc35
Huragan11 <hubert.ragan11@gmail.com>
2025-10-30 06:25:34 +0100

    Add pointcut expression

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/huragan11/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/huragan11/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,19 +2,24 @@ package com.huragan11.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
+    @Pointcut("execution(* com.huragan11.aopdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
 
-//    @Before("execution(public void addAccount())")
-//    @Before("execution(public void com.huragan11.aopdemo.dao.AccountDAO.addAccount())")
-//    @Before("execution(public void add*)")
-//    @Before("execution(void add*)")
-    @Before("execution(* com.huragan11.aopdemo.dao.*.*(..))")
+    @Before("forDaoPackage()")
     public void beforeAddAccount() {
         System.out.println("\n =====>>>>> Executing @Before advice");
     }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics(){
+        System.out.println("\n =====>>>>> Performing API Analytics");
+    }
+
 }
